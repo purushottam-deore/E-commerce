2cdd9baae058f36ae367a82d7df6b2a4bc92691b
purushottam deore <rajput.purushottamp@gamil.com>
2026-04-24 10:56:54 +0530

    fix cors

--- server/server.js
+++ server/server.js
@@ -27,10 +27,20 @@ mongoose
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const allowedOrigins = [
+  "http://localhost:5174",
+  "https://e-commerce-five-sigma-85.vercel.app"
+];
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL,
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
